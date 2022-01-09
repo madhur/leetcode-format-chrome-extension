16c3a08af670ff6a236c5b84a8b6121f344a7f1d
Madhur Ahuja <madhur@users.noreply.github.com>
2022-01-09 12:50:48 +0530

    refactor: added support for C++ (#3)
    Co-authored-by: Madhur Ahuja <madhur.ahuja@games24x7.com>

--- content.js
+++ content.js
@@ -1,23 +1,33 @@
+
+
 let s = document.createElement("script");
+
+// Install beautify
+s.type = "module";
+s.src = chrome.runtime.getURL("beautify.js");
+(document.head || document.documentElement).appendChild(s);
+
+// install script.js
+s = document.createElement("script");
 s.type = "module";
 s.src = chrome.runtime.getURL("script.js");
 (document.head || document.documentElement).appendChild(s);
 
 s = document.createElement("script");
-
 s.src = chrome.runtime.getURL("standalone.js");
 (document.head || document.documentElement).appendChild(s);
 
 s = document.createElement("script");
-
 s.src = chrome.runtime.getURL("parser-babel.mjs");
 s.type = "module";
 (document.head || document.documentElement).appendChild(s);
 
+s = document.createElement("script");
 s.src = chrome.runtime.getURL("parser-typescript.mjs");
 s.type = "module";
 (document.head || document.documentElement).appendChild(s);
 
+s = document.createElement("script");
 s.src = chrome.runtime.getURL("parser-java.js");
 s.type = "module";
 (document.head || document.documentElement).appendChild(s);
--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function startLoading() {
         // codeMirror not found
         return;
     }
-    
+
     let programmingLanguage = document.querySelector(
         ".ant-select-selection-selected-value"
     );
@@ -34,13 +34,13 @@ function startLoading() {
         return;
     }
 
-    if (document.getElementById('format-button') !== null) {        
+    if (document.getElementById('format-button') !== null) {
         return;
     }
     else {
         console.debug('installing button');
     }
-    
+
     let button = getFormatButton();
     button.addEventListener("click", function () {
         formatCodeFinal(codeMirror, programmingLanguage);
@@ -94,14 +94,18 @@ const formatCodeFinal = function (codeMirror, programmingLanguage) {
         });
     } else if (language === "Java") {
         formattedCode = prettierFormat_formatCode.formatCode(codeText, {
-          
-                printWidth: 200,
-                tabWidth: 4
-        
+            printWidth: 200,
+            tabWidth: 4
         });
+    } else if (language === "C++") {
+        formattedCode = js_beautify(codeText, {
+            'indent_size': 4,
+            'brace_style': 'expand'
+        });
+        formattedCode = applyCustomRules(formattedCode);
     }
     else {
-        console.debug(`Formatter not available for ${programmingLanguage.title}`);    
+        console.debug(`Formatter not available for ${programmingLanguage.title}`);
         return;
     }
 
@@ -111,4 +115,156 @@ const formatCodeFinal = function (codeMirror, programmingLanguage) {
     console.debug(`Code formatted for ${programmingLanguage.title}`);
 };
 
-let timer = setInterval(startLoading, 5000);
+const applyCustomRules = function (formatted) {
+    return formatted.replace(/\}\r\n/g, '}\n\n')
+        .replace(/\<\s([a-zA-Z0-9_,: *&<>]+)\s>/g, '<$1>')
+        .replace(/\<\s([a-zA-Z0-9_,: *&<>]+)>/g, '<$1>')
+        .replace(/\<([a-zA-Z0-9_:*]+)\s>/g, '<$1>')
+        .replace(/iterator\s</g, 'iterator<')
+
+        .replace(/ = {\s*([0-9 ,-.]+)\s+};/g, ' = { $1 };')
+        .replace(/\n\s*\n/g, '\n\n')
+        .replace(/,\n\n/g, ',\n')
+        .replace(/\r\n\t{}/g, ' {}')
+        .replace(/\{\r\n\n/g, '{\r\n')
+        .replace(/\r\n\tconst & /g, ' const &')
+        .replace(/,\s+const /g, ', const ')
+        .replace(/#\r\ndefine/g, '#define')
+        .replace(/#\ndefine/g, '#define')
+        .replace(/# define/g, '\r\n#define')
+        .replace(/;#define/g, ';\r\n#define')
+        .replace(/#define/g, '\n#define')
+        .replace(/\n\s*\n#define/g, '\n#define')
+        .replace(/;\r\n#define/g, ';\r\n\r\n#define')
+        .replace(/;\n#define/g, ';\n\n#define')
+        .replace(/\r\n#include/g, '#include')
+        .replace(/\n#include/g, '#include')
+        .replace(/([a-zA-Z0-9\t ./<>?;:"'`!@#$%^&*()\[\]{}_+=|\\-]+)#include/g, '$1\r\n#include')
+        .replace(/vector </g, 'vector<')
+        .replace(/set </g, 'set<')
+        .replace(/map </g, 'map<')
+        .replace(/queue </g, 'queue<')
+        .replace(/stack </g, 'stack<')
+        .replace(/stack </g, 'stack<')
+        .replace(/deque </g, 'deque<')
+        .replace(/list </g, 'list<')
+        .replace(/array </g, 'array<')
+        .replace(/ - > /g, '->')
+        .replace(/\(\s+{\s+/g, '({ ')
+        .replace(/\s+\}\)/g, ' })')
+        .replace(/\tpublic_colon/g, 'public:')
+        .replace(/\tprivate_colon/g, 'private:')
+        .replace(/\tprotected_colon/g, 'protected:')
+
+        .replace(/^#define(.*)$/, '#define')
+
+        .replace(/xxxx/g, 'const')
+        .replace(/\*(\s+)const/g, '*const')
+
+        .replace(/operator (\W+) /g, 'operator$1')
+        .replace(/operator<= >/g, 'operator<=>')
+        .replace(/=(\s+)default/g, '= default')
+        .replace(/; \}/g, ';\n}')
+        .replace(/{\n\t\t\t/g, '{ ')
+        .replace(/= { {/g, '= {\n\t\t{')
+        .replace(/} };/g, '}\n\t};')
+
+        .replace(/(\W+)\* /g, '$1*')
+        .replace(/;\*/g, '; *')
+        .replace(/(\w+) \*(\w+);/g, '$1 * $2;')
+        .replace(/(\w+) \*(\w+)\)/g, '$1 * $2)')
+        .replace(/(\w+) \*(\w+)\(/g, '$1 * $2(')
+        .replace(/(\w+)(\s*)\*(\w+)(\s*)\</g, '$1 * $3 <')
+        .replace(/(\w+)(\s*)\*(\w+)(\s*)\>/g, '$1 * $3 >')
+        .replace(/(\w+)(\s*)\*(\w+)(\s*)\=/g, '$1 * $3 =')
+        .replace(/(\d+)(\s*)\*(\d+)/g, '$1 * $3')
+
+        .replace(/(\W) \* (\w)/g, '$1 *$2')
+        .replace(/->\* /g, '->*')
+        .replace(/ \[ &/g, ' [&')
+        .replace(/\r\n\r\nusing/g, '\r\nusing')
+        .replace(/\n\nusing/g, '\nusing')
+        .replace(/\s,\s/g, ', ')
+        .replace(/> ::/g, '>::')
+
+        .replace(/(\s+)&\s+/g, '$1&')
+        .replace(/\s\[/g, '[')
+        .replace(/\(\s/g, '(')
+        .replace(/\s\)/g, ')')
+
+        .replace(/int \* /g, 'int *')
+        .replace(/char \* /g, 'char *')
+        .replace(/double \* /g, 'double *')
+        .replace(/float \* /g, 'float *')
+        .replace(/bool \* /g, 'bool *')
+        .replace(/void \* /g, 'void *')
+        .replace(/wchar_t \* /g, 'wchar_t *')
+
+        .replace(/(\w+) \*\* /g, '$1 **')
+
+        .replace(/\((\w+) \*\)/g, '($1*)')
+        .replace(/(\w+) \*\>/g, '$1*>')
+
+        .replace(/(\s)\<= /g, '$1 <= ')
+
+        .replace(/\((\w+) &(\w+)\)/g, '($1 & $2)')
+        .replace(/\[(\w+) &(\w+)\]/g, '[$1 & $2]')
+
+        .replace(/\s<\s/g, '<')
+        .replace(/\s<([^<])/g, '<$1')
+        .replace(/([A-Za-z0-9_,\.\(\)\[\]\-\>]+)<([A-Za-z0-9_,\.\(\)\[\]\-\>]+)([\s\;\)])/g, '$1 < $2$3')
+
+        .replace(/<(\s+)const/g, '<const')
+
+        .replace(/#include</g, '#include <')
+        .replace(/#include < /g, '#include <')
+        .replace(/(\w)\> /g, '$1 > ')
+        .replace(/(\w)\>= /g, '$1 >= ')
+        .replace(/\s+{}/g, ' {}')
+        .replace(/\s+{\s+}/g, ' {}')
+
+        .replace(/\s\<\s(\w+)\s\*,/g, '<$1*,')
+        .replace(/\[ \*/g, '[*')
+
+        .replace(/\<(\w+)\s\>/g, '<$1>')
+        .replace(/, (\w+)\s\>/g, ', $1>')
+
+        .replace(/\/\/TEMPLATE/g, 'template <')
+        .replace(/\[ = \]/g, '[=]')
+        .replace(/\}\n\n}/g, '}\n}')
+        .replace(/\}\n\n(\s*)\}/g, '}\n$1}')
+        .replace(/\}\n\n(\s+)\}/g, '}\n$1}')
+
+        .replace(/\}\n\n(\s+)else/g, '}\n$1else')
+
+        .replace(/\n\}\)\;/g, '\n\t});')
+        .replace(/\,\[/g, ', [')
+
+        .replace(/\;\n\n(\s+)\}/g, ';\n$1}')
+
+        .replace(/(\s+)\{([ \t]+)(\w+)/g, '$1{$1\t$3')
+        .replace(/(\s+)\{([ \t]+)\/\//g, '$1{$1$2//')
+        .replace(/=\s{(\s+)/g, '= { ')
+
+        .replace(/\{\r\n\s+([0-9,-\s.]+)\r\n\s+\}/g, '{ $1 }')
+        .replace(/\{\n\s+([0-9,-\s.]+)\n\s+\}/g, '{ $1 }')
+        .replace(/\{ \{/g, '{\n\t\t\{')
+        .replace(/ \/\//g, '\t//')
+
+        .replace(/(['"])(\s+)\}/g, '$1 }')
+        .replace(/(\w+) \* (\w+) =/g, '$1 *$2 =')
+        .replace(/(\w+) \* (\w+)\)/g, '$1 *$2)')
+        .replace(/(\w+) \* (\w+)\(/g, '$1* $2(')
+
+        .replace(/(\w+) \*\& (\w+)/g, '$1* &$2')
+
+        .replace(/\s\<\s(\w+)\s\>/g, '<$1>')
+        .replace(/\s\<\s(\w+)\,/g, '<$1,')
+        .replace(/\{\}~/g, '{}\n\t~')
+        .replace(/_cast </g, '_cast<')
+
+        .replace(/\>\s+\{\s*([A-Za-z0-9 ,-.\"]+)\s+\}\;/g, '> { $1 };');
+}
+
+
+setInterval(startLoading, 5000);
